9d5e8f4800b9e5329ff351ccc492ffaff1f09be2
Douglas Bello <dgbello.dev@hotmail.com>
2024-09-24 15:58:59 -0300

    first commit

---
+++ src/main/java/br/com/douglasbello/redis_problem/RedisProblemApplication.java
@@ -0,0 +1,13 @@
+package br.com.douglasbello.redis_problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisProblemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/douglasbello/redis_problem/RedisProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.douglasbello.redis_problem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisProblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
